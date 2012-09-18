5e54d175497c558287d2c378eb09baff6e01da1b
Eduardo Grajeda <tatofoo@gmail.com>
2012-09-18 14:32:27 -0300

    fixing main code, all tests passing

--- lib/sc2_achievements.rb
+++ lib/sc2_achievements.rb
@@ -1,40 +1,9 @@
-# encoding: UTF-8
 require 'nokogiri'
 require 'open-uri'
+require 'sc2_achievements/homepage'
 
 module SC2Achievements
-  class << self
-    def self.for(user_path)
-      recent_achievements_of(user_path)
-    end
-
-  private
-    def self.path_for(user_path)
-      "http://battle.net/sc2/en/profile#{user_path}/achievements/"
-    end
-
-    def self.recent_achievements_of(user_path)
-      achievements = []
-      page = fetch_page(path_for(user_path))
-      page.css('#recent-achievements a').each do |achievement|
-        description = extract_text_of(achievement.attr('data-tooltip'))
-        date = Date.strptime(achievement.css('span').text.strip, '%m/%d/%Y')
-        achievements.push({
-          :title       => achievement.xpath('text()').text.strip,
-          :description => description,
-          :date        => date.strftime('%Y-%m-%d')
-        })
-      end
-      achievements
-    end
-
-    def self.fetch_page(path)
-      html = open(path).read
-      @current_page = Nokogiri::HTML(html)
-    end
-
-    def self.extract_text_of(css)
-      @current_page.css(css).xpath('text()').text.strip
-    end
+  def self.for(user_path)
+    Homepage.get_achievements_for(user_path)
   end
 end
