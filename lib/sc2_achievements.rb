9f18fc049c8e40fb01185d16d1cd340fe0a76dca
Eduardo Grajeda <tatofoo@gmail.com>
2012-09-21 00:15:32 -0300

    refactoring the achievements retrieval code

--- lib/sc2_achievements.rb
+++ lib/sc2_achievements.rb
@@ -20,15 +20,15 @@ private
   def self.get_achievements_for(user_path)
     achievements = Page.get_achievements_for user_path
     Page.get_categories_for(user_path).inject(achievements) do |achievements, category|
+      add_subcategories_achievements_to(achievements, user_path, category)
+    end
+  end
+
+  def self.add_subcategories_achievements_to(achievements, user_path, category)
+    Page.get_categories_for(user_path, :category => category).inject(achievements) do |achievements, category|
       category_achievements = Page.get_achievements_for(user_path, :category => category)
-      achievements.merge(category_achievements) do |title, old, new|
-        old.merge(new)
-      end
-      Page.get_categories_for(user_path, :category => category).inject(achievements) do |achievements, category|
-        category_achievements = Page.get_achievements_for(user_path, :category => category)
-        achievements.merge(category_achievements) do |title, old, new|
-          old.merge(new)
-        end
+      achievements.merge(category_achievements) do |title, old_achievement, new_achievement|
+        old_achievement.merge(new_achievement)
       end
     end
   end
